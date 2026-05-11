9f25d03a19df781eb8977d9bd3ae3d51679f01cc
dakshna11 <dakshna511@gmail.com>
2026-05-11 23:30:32 +0530

    Landing page UI changes

--- task-tracker/app/page.tsx
+++ task-tracker/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
+import styles from '@/styles/home.module.scss';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-2xl font-bold">
-        Task Tracker App
+    <>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Task <span className={styles.highlight}>Flow</span>
       </h1>
+      <div className={styles.btnContainer}>
+        <button className={styles.signUp}>Sign Up</button>
+        <button className={styles.logIn}>Log In</button>
+      </div>
     </div>
+    <h2 className={styles.description}>Your ultimate task management <span className={styles.highlight}>solution</span>!</h2>
+    </>
   );
 }
